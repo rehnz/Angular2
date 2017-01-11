8e950a6ddbdb90e026ac6afb193cc8a1212d95f1
Rene Martinez <renzp2@gmail.com>
2017-01-11 00:41:28 -0800

    Inital Commit 20170111

---
+++ angular/start/app/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'FrontHeader',
+  templateUrl: 'app/header/header.html'
+})
+export class HeaderComponent {
+  
+   header1 = 'A site of nothing.';
+   header2 = 'suppaaaaa!';
+   headerBtn = 'Click Homie!';
+ }
